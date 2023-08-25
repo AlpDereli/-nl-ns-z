6cb15df352fd70df1beebe07c834836b12f8842d
AlpDereli <alpdereli26@gmail.com>
2023-08-25 20:34:21 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+char[] ünlü = new char[] {'e','E','u','U','i','I','o','O','a','A'};
+Console.WriteLine("Please enter a word/words: ");
+string s = Console.ReadLine();
+string[] arr = s.Split(" ");
+foreach (string str in arr)
+{
+    int i = 0;
+    for (int j = 0; j < str.Length-1; j++)
+    {
+        if (!ünlü.Contains(str[j])&& !ünlü.Contains(str[j+1]))
+        {
+            i += 1;
+        }
+
+    }
+    if (i>0)
+    {
+        Console.Write("False ");
+    }
+    else
+    {
+        Console.Write("True ");
+    }
+}
